9fced52be78e4a52c07d81c1efe882cbf1ac29e0
RandaAsim <randasanfora2015.com>
2023-11-27 13:17:40 -0800

    C again

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - Checks
+ * @list: linked list
+ * Return: 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *new, *prev;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	new = list->next;
+	prev = list->next->next;
+
+	while (new && prev && prev->next)
+	{
+		if (new == prev)
+			return (1);
+
+		new = new->next;
+		prev = prev->next->next;
+	}
+
+	return (0);
+}
