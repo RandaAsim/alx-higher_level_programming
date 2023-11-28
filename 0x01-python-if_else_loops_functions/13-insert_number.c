c0fd92274f8da4ae06dc7531e195272c9bc33a12
RandaAsim <randasanfora2015.com>
2023-11-28 12:16:18 -0800

    py loop task

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+#include<stdio.h>
+/**
+ * insert_node : function.
+ *
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *prev, *node, *new;
+
+	node = *head;
+	if (node == NULL)
+	{
+		new = malloc(sizeof(listint_t));
+		new->n = number;
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+	prev = node;
+	node = node->next;
+
+
+	if (prev->n > number)
+	{
+		new = malloc(sizeof(listint_t));
+		new->n = number;
+		new->next = prev;
+		*head = new;
+		return(new);
+	}
+	while (node)
+	{
+		if (node->n > number)
+		{
+			new = malloc(sizeof(listint_t));
+			new->n = number;
+			prev->next = new;
+			new->next = node;
+			return(new);
+		}
+		prev = node;
+		node = node->next;
+	}
+	new = malloc(sizeof(listint_t));
+	prev->next = new;
+	new->n = number;
+	new->next = NULL;
+
+	return(new);
+}
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+i = 97
+print("{}".format("".join(chr(i) for i in range(97, 123))), end="")
