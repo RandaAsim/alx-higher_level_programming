25ddcad4e9997b558ce5b989de1975fe85aedd7b
RandaAsim <randasanfora2015.com>
2023-11-30 14:47:30 -0800

    import and handel oprations + test file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,12 +7,15 @@ if __name__ == "__main__":
     
     from sys import argv
 
-    count = len(sys.argv) - 1
-    if count == 0:
+    x = len(argv)
+    if x == 1:
         print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
     else:
-        print("{} arguments:".format(count))
-        for i in range(count):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        x = x - 1
+        print("{} ".format(x), end="")
+        if x == 1:
+            print("argument:")
+        else:
+            print("arguments:")
+            for i in range(1, len(argv)):
+                print("{}: {}".format(i, argv[i]))
