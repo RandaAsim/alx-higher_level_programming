712bb0ec0b59471af58145ec49fd48e84e1cf7cf
RandaAsim <randasanfora2015.com>
2023-11-30 14:31:54 -0800

    import and handel oprations + test file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+a = 10
+b = 5
+
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
-    import sys
+    
+    from sys import argv
 
     count = len(sys.argv) - 1
     if count == 0:
