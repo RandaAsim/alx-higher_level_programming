fb1c8f2bd5e5e8c8cafd7ee5e20cdf28558508c5
RandaAsim <randasanfora2015.com>
2023-11-30 15:06:44 -0800

    import and handel oprations + test file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
+a = 10
+b = 5
+total = 0
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    from  sys import argv
-
-    a = 10
-    b = 5
-    total = 0
+    from sys import argv
     for i in range(1, len(argv)):
         total += int(argv[i])
         print("{:d}".format(sum))
