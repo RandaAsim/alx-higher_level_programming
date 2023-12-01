8b52561f9e94e789ebacf8c6a2b02bf8f4535113
RandaAsim <randasanfora2015.com>
2023-11-30 16:11:18 -0800

    what next

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,9 @@
 
 a = 10
 b = 5
-total = 0
+sum = 0
 if __name__ == "__main__":
     from sys import argv
     for i in range(1, len(argv)):
-        total += int(argv[i])
-        print("{:d}".format(total))
+        sum += int(argv[i])
+    print("{:d}".format(sum))
