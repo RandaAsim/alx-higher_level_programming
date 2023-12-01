e42c09c2209d7cd15b76087fa87a852b2080b155
RandaAsim <randasanfora2015.com>
2023-11-30 16:07:30 -0800

    what next

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     from sys import argv
     for i in range(1, len(argv)):
         total += int(argv[i])
-        print("{:d}".format(sum))
+        print("{:d}".format(total))
