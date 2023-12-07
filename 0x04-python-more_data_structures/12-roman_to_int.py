4a9d3a520a6de0009fdaef050e22cf23ad1714bf
RandaAsim <randasanfora2015.com>
2023-12-06 18:59:05 -0800

    set

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    summ = 0
+    if isinstance(roman_string, str):
+        units = {'I': 1, 'V': 5, 'X': 10}
+        units.update({'L': 50, 'C': 100,  'D': 500, 'M': 100})
+        strr = ''.join(reversed(roman_string))
+        for i in range(0, len(strr)):
+            if units.get(strr[i]):
+                if i == 0:
+                    summ += units.get(strr[i])
+                elif units.get(strr[i - 1]) > units.get(strr[i]):
+                    summ -= units.get(strr[i])
+                else:
+                    summ += units.get(strr[i])
+    return (summ)
