391ae1bbe3687a49456acace9e1416afee305cfc
RandaAsim <randasanfora2015@gmail.com>
2024-03-16 05:30:21 -0700

    good

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,6 +3,6 @@
 if __name__ == "__main__":
     from add_0 import add
 
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    r = 1
+    n = 2
+    print("{} + {} = {}".format(a, b, add(r, n)))
